362bb6d5eaf55ec91ea5264676ebdd0328509044
raj073 <rajoni.das@outlook.com>
2023-07-14 17:17:07 +0600

    Initial server is being setup

--- index.js
+++ index.js
@@ -1,7 +1,41 @@
 const express = require("express");
-const { mongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const cors = require("cors");
+const app = express();
 
 require("dotenv").config();
 
 const port = process.env.PORT || 5000;
+
+//Middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mlxaon5.mongodb.net/?retryWrites=true&w=majority`;
+
+console.log(uri);
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+const run = async () => {
+  try {
+    const db = client.db("tech-net");
+  } finally {
+  }
+};
+
+run().catch((error) => console.log(error));
+
+app.get("/", (req, res) => {
+  res.send("e-Book Desk Server is Running");
+});
+
+app.listen(port, () => {
+  console.log(`e-Book Desk Server is Running on ${port}`);
+});
